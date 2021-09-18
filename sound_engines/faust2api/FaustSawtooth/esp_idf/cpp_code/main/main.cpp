bbcc7467d0c2e07c2e9f7ef203aa3475ecccabcb
Goofy2k <goofy2k@gmail.com>
2021-09-19 00:21:21 +0200

    some more builds

--- sound_engines/faust2api/FaustSawtooth/esp_idf/cpp_code/main/main.cpp
+++ sound_engines/faust2api/FaustSawtooth/esp_idf/cpp_code/main/main.cpp
@@ -45,8 +45,38 @@ void app_main(void)
     if (DSP->isRunning()) {printf("AFTER START RUNNING\n");} else {printf("AFTER START NOT RUNNING\n");} ;
   
     //printf("Hello modified 3x world!\n");
-    DSP->setParamValue("freq",880);
-    
+    //DSP->setParamValue("freq",220);
+    //DSP->keyOn(50,50);
+    DSP->allNotesOff();
+    uintptr_t myvoice0 = DSP->newVoice();
+     uintptr_t myvoice1 = DSP->newVoice();
+     float CPULoad = DSP->getCPULoad();
+     printf("CPULoad %7.5f \n",CPULoad);
+     
+     float min0 = DSP->getParamMin(0);
+     float max0 = DSP->getParamMax(0);
+     float init0 = DSP->getParamInit(0);
+     float min1 = DSP->getParamMin(1);
+     float max1 = DSP->getParamMax(1);
+     float init1 = DSP->getParamInit(1); 
+     printf("min0 = %7.5f \n",min0);
+     printf("max0 = %7.5f \n",max0);
+     printf("init0 = %7.5f \n",init0);
+     printf("min1 = %7.5f \n",min1);
+     printf("max1 = %7.5f \n",max1);
+     printf("init1 = %7.5f \n",init1);
+
+    float freq0 = DSP-> getVoiceParamValue(0, 0);
+    float gain0 = DSP-> getVoiceParamValue(1, 0);
+    float freq1 = DSP-> getVoiceParamValue(0, 1);
+    float gain1 = DSP-> getVoiceParamValue(1, 1);
+    printf("freq0 = %7.5f \n",freq0);
+    printf("gain0 = %7.5f \n",gain0);
+    printf("freq1 = %7.5f \n",freq1);
+    printf("gain1 = %7.5f \n",gain1);
+
+
+    //DSP->setVoiceParamValue(0,myvoice0,220);  //RUNTIME ERROR
     /*
     while(1) {
         //printf("Hello modified 4x world!\n");
