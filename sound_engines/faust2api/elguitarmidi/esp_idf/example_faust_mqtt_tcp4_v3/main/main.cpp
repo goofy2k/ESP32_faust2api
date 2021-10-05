7bf2fc85f53201d165a1654ae4ef8046f207cffb
Goofy2k <goofy2k@gmail.com>
2021-10-05 18:51:59 +0200

    .

--- sound_engines/faust2api/elguitarmidi/esp_idf/example_faust_mqtt_tcp4_v3/main/main.cpp
+++ sound_engines/faust2api/elguitarmidi/esp_idf/example_faust_mqtt_tcp4_v3/main/main.cpp
@@ -145,26 +145,7 @@ void wifi_init_sta(void)
                                                         &event_handler,
                                                         NULL,
                                                         &instance_got_ip));
-    /*
-    wifi_config_t wifi_config = {
-        .sta = {
-            //.ssid = EXAMPLE_ESP_WIFI_SSID,
-            .ssid = "Verhoeckx_glas",
-            //.password = EXAMPLE_ESP_WIFI_PASS,
-            .password = "Goofy2kmacho_99",
-            //Setting a password implies station will connect to all security modes including WEP/WPA.
-            // However these modes are deprecated and not advisable to be used. Incase your Access point
-            // doesn't support WPA2, these mode can be enabled by commenting below line
-	     .threshold.authmode = WIFI_AUTH_WPA2_PSK,
-
-            .pmf_cfg = {
-                .capable = true,
-                .required = false
-            },
-        },
-    };
-    
-    */
+ 
     
     //https://esp32.com/viewtopic.php?t=1317
     //You can init structs in C++ but all elements of the struct has to be included in the initialization in the order they are declared as you can not reference properties by name. 
